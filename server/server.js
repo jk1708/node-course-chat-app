e82d3620532f79bacbc3266459329ff8f4bbc024
Jaishree Khatri <jaishreekhatri17@gmail.com>
2019-01-22 23:10:59 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-const path= require('path');
-const express= require('express');
-const publicPath=  path.join(__dirname, '../public');
-console.log(__dirname, '/../public');
-var app= express();
-const port= process.env.PORT ||3000;
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
 app.use(express.static(publicPath));
-app.listen(3000, ()=>{
-  console.log(`Server is up on port ${port}`);
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
