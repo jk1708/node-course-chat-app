b7f345573c388b109dab48cff626fa1cde1ac058
Jaishree Khatri <jaishreekhatri17@gmail.com>
2019-01-27 12:12:57 +0530

    add User class and update event listeners

--- public/js/chat.js
+++ public/js/chat.js
@@ -26,13 +26,34 @@ var newMessage= messages.children('li:last-child')
   }
 }
 socket.on('connect', function () {
-  console.log('Connected to server');
+
+var params= jQuery.deparam(window.location.search);
+socket.emit('join', params, function(err){
+  if(err)
+  {
+
+    alert(err);
+    window.location.href='/';
+  }
+  else {
+    console.log('no error');
+  }
+});
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
-});
 
+});
+socket.on('updateUserList', function (users){
+  //console.log('heyyyyyyyy');
+  var ol=jQuery('<ol></ol>');
+  users.forEach(function (user){
+    ol.append(jQuery('<li></li>').text(user));
+  });
+  jQuery('#users').html(ol);
+  console.log(users);
+});
 socket.on('newMessage', function (message) {
 
   var formattedTime= moment(message.createdAt).format('h:mm a');
@@ -61,12 +82,7 @@ var html= Mustache.render(template,{
     jQuery('#messages').append(html);
       scrollToBottom();
 });
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'hey ross'
-}, function(data){
-  console.log('got it', data);
-});
+
 var messageTextBox= jQuery('[name=message]');
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
